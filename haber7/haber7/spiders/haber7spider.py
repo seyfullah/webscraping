64ba343770ee4f0b3049bb6c96bb1908ac0d7230
Seyfullah Tıkıç <stikic@gmail.com>
2024-04-04 19:12:39 +0300

    Second commit

--- haber7/haber7/spiders/haber7spider.py
+++ haber7/haber7/spiders/haber7spider.py
@@ -7,10 +7,17 @@ class Haber7spiderSpider(scrapy.Spider):
     start_urls = ["https://www.haber7.com"]
 
     def parse(self, response):
-        subheaders = response.css('div.subhead-slider a')
         
+        subheaders = response.css('div.subhead-slider a')
         for subheader in subheaders:
             yield {
                 'title' : subheader.css('div.title ::text').get(),
                 'url' : subheader.css('a').attrib['href']
+            }
+        
+        headlinesections = response.css('div.headline-slider a.headline-slider-item')
+        for headlinesection in headlinesections:
+            yield {
+                'title' : headlinesection.css('div.title ::text').get(),
+                'url' : headlinesection.css('a').attrib['href']
             }
